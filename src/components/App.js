eda66eb4841f6dc184a95b667a3ca795c576efeb
rutvik-pimple <48850558+rutvik-pimple@users.noreply.github.com>
2020-10-31 14:02:30 +0530

    .. ..

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  constructor() {
+    super();
+    this.state = { time: new Date() };
+  }
+  currentTime() {
+    this.setState({ time: new Date() });
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.currentTime(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <>
+        <div className="Clock">
+          <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
+function generateCodeFromObject(obj) {
+  //return a code generated string
+}
 
+module.exports = generateCodeFromObject;
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
